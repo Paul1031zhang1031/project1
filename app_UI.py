bed25549dee2c8cc64c329fc903b173d45a87d06
jiechen <chenjiealice1031@gmail.com>
2025-08-14 23:09:37 -0400

    change the qa model way

--- app_UI.py
+++ app_UI.py
@@ -39,7 +39,7 @@ BANNER_PATH = "./images/bishop_logo_2.jpg"
 BANNER_HEIGHT = 150
 
 # QA_MODELS_TO_EVALUATE = ["gemma2-9b-it", "llama3-8b-8192", "llama3-70b-8192"]
-QA_MODELS_TO_EVALUATE = "llama3-70b-8192"
+QA_MODELS_TO_EVALUATE = ["llama3-70b-8192"]
 # Based on Phase 1 results, llama3-8b is the best for summarization. We will use it exclusively.
 SUMMARY_MODEL = "llama3-70b-8192" 
 
